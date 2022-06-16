af88fb917ed18cc31ff4d13ab5ec4ca9bcb54419
ThomasDCW <thomas.dicola@hotmail.fr>
2022-06-16 11:07:22 +0200

    started HomeNavigation

---
+++ frontend/src/components/HomeNavigation/index.jsx
@@ -0,0 +1,9 @@
+import HomeCard from "@components/HomeCard";
+
+export default function NavigationHome() {
+  return (
+    <div>
+      <HomeCard />
+    </div>
+  );
+}
--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -1,10 +1,11 @@
 import Header from "@components/Header";
+import HomeNavigation from "@components/HomeNavigation";
 
 export default function HomePage() {
   return (
     <>
       <Header />
-      HomePage
+      <HomeNavigation />
     </>
   );
 }
