5635f476e8e31ffa1c774eeeb340244dd9e124eb
ThomasDCW <thomas.dicola@hotmail.fr>
2022-06-16 15:05:28 +0200

    add routes

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,6 +1,7 @@
 import Footer from "@components/Footer";
 import Header from "@components/Header";
 import HomePage from "@pages/Home";
+import Pages from "@pages/Pages";
 import { Route, Routes } from "react-router-dom";
 
 function App() {
@@ -9,6 +10,7 @@ function App() {
       <Header />
       <Routes>
         <Route path="/" element={<HomePage />} />
+        <Route path="/:id" element={<Pages />} />
       </Routes>
       <Footer />
     </div>
