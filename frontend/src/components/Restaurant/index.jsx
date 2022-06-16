59a60b95051bbcc5ebb90f7d04cb87b6301f990f
ThomasDCW <thomas.dicola@hotmail.fr>
2022-06-16 22:56:43 +0200

    create object for useparams

---
+++ frontend/src/components/Restaurant/index.jsx
@@ -0,0 +1,39 @@
+import propTypes from "prop-types";
+import SRestaurant from "./style";
+
+export default function Restaurant({
+  label,
+  description,
+  location,
+  phonenumber,
+  pic1,
+  pic2,
+  pic3,
+}) {
+  return (
+    <SRestaurant>
+      <h1>{label}</h1>
+      <article>
+        <div className="infoscontainer">
+          <p>{description}</p>
+          <h2>{location}</h2>
+          <h2>{phonenumber}</h2>
+        </div>
+        <div className="picturecontainer">
+          <img src={pic1} alt="picture1" />
+          <img src={pic2} alt="picture2" />
+          <img src={pic3} alt="picture2" />
+        </div>
+      </article>
+    </SRestaurant>
+  );
+}
+Restaurant.propTypes = {
+  label: propTypes.string.isRequired,
+  description: propTypes.string.isRequired,
+  location: propTypes.string.isRequired,
+  phonenumber: propTypes.string.isRequired,
+  pic1: propTypes.string.isRequired,
+  pic2: propTypes.string.isRequired,
+  pic3: propTypes.string.isRequired,
+};
